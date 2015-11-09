56921870034a83cd3aa2fe1dc2a6327f956b99d3
risensh1ne <risensh1ne@naver.com>
2015-11-10 00:15:43 +0900

    11/9

--- Assets/Scripts/TowerController.cs
+++ Assets/Scripts/TowerController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class TowerController : Photon.MonoBehaviour, IPlayer {
 
-	public float health = 500.0f;
+	public float health = 100.0f;
 	public float attackRange = 8.0f;
 	public float attackRate = 3.0f;
 	public bool bAttacking = false;
@@ -14,7 +14,16 @@ public class TowerController : Photon.MonoBehaviour, IPlayer {
 	public GameObject targetEnemy;
 	private GameObject lastAttackedBy;
 
-	Vector2 healthBarSize = new Vector2(50, 5);
+    private Color origObjColor;
+
+    private GameObject destructionEffect;
+
+    private float elapsedTimeOfDistruction = 0;
+    private float durationOfDistruction = 2.0f;
+    private float heightOfDistruction = 1.0f;
+    private float alphaOfDistruction = 1.0f;
+
+    Vector2 healthBarSize = new Vector2(50, 5);
 	
 	public Texture2D progressBarBack;
 	public Texture2D progressBarHealth;
@@ -58,17 +67,29 @@ public class TowerController : Photon.MonoBehaviour, IPlayer {
 		
 		if (health <= 0)
         {
-            Destroy(gameObject);
+            DestructTower();
         }
 			
 	}
 
    
+    void DestructTower()
+    {
+        Color origObjColor = GetComponent<MeshRenderer>().material.color;
+
+        destructionEffect = ObjectPool.instance.GetObjectForType("Destruction01", true);
+        if (destructionEffect != null)
+        { 
+            destructionEffect.transform.position = transform.position;
+            destructionEffect.GetComponent<ParticleSystem>().Play();
+        }
+        isDead = true;
+    }
 
 	// Use this for initialization
 	void Start () {
-		firePoint = transform.Find ("TowerFirePoint");
-	}
+		firePoint = transform.Find ("TowerFirePoint");   
+    }
 
 	void OnGUI()
 	{
@@ -94,33 +115,64 @@ public class TowerController : Photon.MonoBehaviour, IPlayer {
 		float enemyDistance = 0;
 		Vector3 direction;
 
-		Collider[] cldrs = Physics.OverlapSphere (firePoint.position, attackRange);
-		if (cldrs.Length > 0 && !isAttacking) {
-			for (int i=0; i < cldrs.Length; i++) {
-				
-				bool isTarget = false;
-				if (cldrs [i].tag == "Player" || cldrs [i].tag == "minion") {
-					IPlayer ip = cldrs [i].gameObject.GetComponent<IPlayer>();
-					if (attachedTeam != ip.checkTeam() && !ip.isDead)
-						isTarget = true;
-				}
-				
-				if (isTarget) {
-					float dist = Vector3.Distance (cldrs [i].transform.position, transform.position);
-					if (enemyDistance == 0 || dist < enemyDistance) {
-						enemyDistance = dist;
-						enemyIndex = i;
-					}
-				}
-			}
-		}
-		
-		if (enemyIndex >= 0) {
-			targetEnemy = cldrs [enemyIndex].gameObject;
-			IPlayer ip = targetEnemy.GetComponent<IPlayer>();
-			if (!ip.isDead)
-				gameObject.GetComponent<PhotonView> ().RPC ("attack", PhotonTargets.All, targetEnemy.transform.position);
-		}
+        if (isDead)
+        {
+            float newY = transform.position.y - heightOfDistruction * Time.fixedDeltaTime;
+                
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+
+            alphaOfDistruction -= Time.fixedDeltaTime / durationOfDistruction;
+            if (alphaOfDistruction < 0)
+                alphaOfDistruction = 0;
+            
+            GetComponent<MeshRenderer>().material.color = new Color(origObjColor.r, origObjColor.g, origObjColor.b, alphaOfDistruction);
+
+            elapsedTimeOfDistruction += Time.fixedDeltaTime;
+
+
+            if (elapsedTimeOfDistruction >= durationOfDistruction)
+            {
+                ObjectPool.instance.PoolObject(destructionEffect);
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+
+            Collider[] cldrs = Physics.OverlapSphere(firePoint.position, attackRange);
+            if (cldrs.Length > 0 && !isAttacking)
+            {
+                for (int i = 0; i < cldrs.Length; i++)
+                {
+
+                    bool isTarget = false;
+                    if (cldrs[i].tag == "Player" || cldrs[i].tag == "minion")
+                    {
+                        IPlayer ip = cldrs[i].gameObject.GetComponent<IPlayer>();
+                        if (attachedTeam != ip.checkTeam() && !ip.isDead)
+                            isTarget = true;
+                    }
+
+                    if (isTarget)
+                    {
+                        float dist = Vector3.Distance(cldrs[i].transform.position, transform.position);
+                        if (enemyDistance == 0 || dist < enemyDistance)
+                        {
+                            enemyDistance = dist;
+                            enemyIndex = i;
+                        }
+                    }
+                }
+            }
+
+            if (enemyIndex >= 0)
+            {
+                targetEnemy = cldrs[enemyIndex].gameObject;
+                IPlayer ip = targetEnemy.GetComponent<IPlayer>();
+                if (!ip.isDead)
+                    gameObject.GetComponent<PhotonView>().RPC("attack", PhotonTargets.All, targetEnemy.transform.position);
+            }
+        }
 	}
 
 	[PunRPC]
@@ -140,6 +192,6 @@ public class TowerController : Photon.MonoBehaviour, IPlayer {
 		yield return new WaitForSeconds(attackRate);
 		isAttacking = false;
 		ObjectPool.instance.PoolObject(fireballObj);
-	}
+    }
 	
 }
